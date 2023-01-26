37453967b6c32f0eb6ca710f03797e3f8e721008
minhyeok0328 <alsgur0008@naver.com>
2023-01-26 17:51:40 +0900

    feat: todo list crud 관련 jsdoc 업데이트

--- src/index.js
+++ src/index.js
@@ -1,51 +1,62 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * 할 일 조회 데이터
+ * @typedef {Object} TodoItem
+ * @property {number} id - 고유번호
+ * @property {string} content - 할 일 내용
+ * @property {string} category - 카테고리
+ * @property {string} tag - 태그
+ * @property {boolean} status - 완료 여부
  */
-function Book(title, author) {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * 할 일을 추가하는 함수
+ * @constructor
+ * @param {Object} data - 할 일 목록 정보
+ * @param {string} data.content - 할 일 내용
+ * @param {string} data.category - 카테고리
+ * @param {string=} data.tag - 태그
+ * @return {boolean}
  */
-function foo() {}
+function create(data) {}
+
+/**
+ * 할 일을 수정하는 함수
+ * @constructor
+ * @param {number} id - 할 일 고유번호
+ * @param {Object} data - 할 일 목록 정보
+ * @param {string=} data.content - 할 일 내용
+ * @param {string=} data.category - 카테고리
+ * @param {string=} data.tag - 태그
+ * @param {string=} data.status - 완료 여부
+ * @return {boolean}
+ */
+function patch(id, data) {}
+
+/**
+ * 할 일을 삭제하는 함수
+ * @constructor
+ * @param {number} id - 할 일 고유번호
+ * @return {boolean}
+ */
+function remove(id) {}
+
+/**
+ * 할 일 목록 전체를 삭제하는 함수
+ * @constructor
+ * @return {boolean}
+ */
+function removeAll() {}
+
+/**
+ * 할 일 목록 중 하나를 가져오는 함수
+ * @param {number} id - 할 일 목록의 고유번호
+ * @return {TodoItem}
+ */
+function findOne(id) {}
 
 // Use the inline {@link} tag to include a link within a free-form description.
 /**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
+ * 할 일 목록 전체를 가져오는 함수
+ * @return {TodoItem[]}
  */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+function findAll() {}
